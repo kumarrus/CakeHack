79aa208eb561179cdfd1b3c896d90ad7da3babf8
Pranay K <pran262@gmail.com>
2013-01-06 12:33:21 -0500

    Added constructor for objectDrop
    Signed-off-by: Pranay K <pran262@gmail.com>

--- DroidGame/src/com/ch/gamedroid/ObjectDrop.java
+++ DroidGame/src/com/ch/gamedroid/ObjectDrop.java
@@ -3,11 +3,16 @@ package com.ch.gamedroid;
 public class ObjectDrop {
 	public int positionX;
 	public int positionY;
+	public int velocityY;
 	
 	public static final int FALLING = 0;
 	public static final int LANDED = 1;
 	public static final int DESTROYED = 2;
 	
-	public int velocityY;
-
+	ObjectDrop(int positionX, int positionY, int velocityY )
+	{
+		this.positionX = positionX;
+		this.positionY = positionX;
+		this.velocityY = velocityY;
+	}
 }
