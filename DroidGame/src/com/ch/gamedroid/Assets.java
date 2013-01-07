814119f6230cd63a188d6ac44ca1f3437da135b3
flux37 <rushab3737@gmail.com>
2013-01-07 02:29:12 -0500

    restore some files to previous versions

--- DroidGame/src/com/ch/gamedroid/Assets.java
+++ DroidGame/src/com/ch/gamedroid/Assets.java
@@ -1,5 +1,13 @@
 package com.ch.gamedroid;
 
+import com.ch.framework.Pixmap;
+
 public class Assets {
+	
+    public static Pixmap ready;
+    public static Pixmap gameOver;
+    public static Pixmap characterOne;
+    public static Pixmap coin_fall;
+    public static Pixmap coin_land;
 
 }
--- DroidGame/src/com/ch/gamedroid/LoadingScreen.java
+++ DroidGame/src/com/ch/gamedroid/LoadingScreen.java
@@ -1,5 +1,51 @@
 package com.ch.gamedroid;
 
-public class LoadingScreen {
+import com.ch.framework.Graphics.PixmapFormat;
+import com.ch.framework.Graphics;
+import com.ch.framework.Game;
+import com.ch.framework.Screen;
+
+public class LoadingScreen extends Screen {
+
+	public LoadingScreen(Game game) 
+	{
+		super(game);
+	}
+
+	@Override
+	public void update(float deltaTime) 
+	{
+		Graphics g = game.getGraphics();
+		Assets.coin_fall = g.newPixmap("coin_falling.png", PixmapFormat.RGB565);
+		Assets.coin_land = g.newPixmap("coin_landed.png", PixmapFormat.RGB565);
+		Assets.characterOne = g.newPixmap("character1.png", PixmapFormat.RGB565);
+		Assets.gameOver = g.newPixmap("gameover.png", PixmapFormat.RGB565);
+		Assets.ready = g.newPixmap("ready.png", PixmapFormat.RGB565);
+		game.setScreen(new GameScreen(game));
+	}
+
+	@Override
+	public void present(float deltaTime) 
+	{
+		
+	}
+
+	@Override
+	public void pause() 
+	{
+		
+	}
+
+	@Override
+	public void resume() 
+	{
+		
+	}
+
+	@Override
+	public void dispose() 
+	{
+		
+	}
 
 }
