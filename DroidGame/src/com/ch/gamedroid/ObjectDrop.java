3b8caec7a505235dc8b6a1511b6c756d48246c30
flux37 <rushab3737@gmail.com>
2013-01-06 21:09:16 -0500

    add character and object class

--- DroidGame/src/com/ch/gamedroid/ObjectDrop.java
+++ DroidGame/src/com/ch/gamedroid/ObjectDrop.java
@@ -1,5 +1,13 @@
 package com.ch.gamedroid;
 
 public class ObjectDrop {
+	public int positionX;
+	public int positionY;
+	
+	public static final int FALLING = 0;
+	public static final int LANDED = 1;
+	public static final int DESTROYED = 2;
+	
+	public int velocityY;
 
 }
