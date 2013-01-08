c692d630ba2c3feeab866ba17967bb71038480af
Pranay K <pran262@gmail.com>
2013-01-07 21:49:19 -0500

    Test version with - coins falling implemented
    Signed-off-by: Pranay K <pran262@gmail.com>

--- DroidGame/src/com/ch/gamedroid/Assets.java
+++ DroidGame/src/com/ch/gamedroid/Assets.java
@@ -1,5 +1,12 @@
 package com.ch.gamedroid;
+import com.ch.framework.Pixmap;
 
 public class Assets {
-
+	
+    public static Pixmap ready;
+    public static Pixmap gameOver;
+    public static Pixmap characterOne;
+    public static Pixmap coin_fall;
+    public static Pixmap coin_land;
+    public static Pixmap background;
 }
